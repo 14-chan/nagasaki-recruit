17913ecdba7bd75107f698f3f6c0c475cbd97892
hamasaki <sltk6ao7@gmail.com>
2024-12-16 13:58:28 +0900

    second commit

---
+++ html/wp-content/themes/nagasaki.city-recruit/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, height=device-height ,initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
+<title><?php echo bloginfo('name'); ?></title>
+<?php wp_head(); ?>
+</head>
+<body class="l-pagebody">
+    <!--ヘッダ-->
+    <header class="g-header">
+    </header>
